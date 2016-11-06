6ae2b7b3b26de44a510c9ec3046adbc0cf5058ef
Axel Hecht <axel@pike.org>
2016-11-06 17:37:54 +0100

    display more values, platforms in tests

--- code.js
+++ code.js
@@ -2,7 +2,7 @@
 const Treeherder = 'https://treeherder.mozilla.org/api/project/';
 const tree = 'try';
 
-let signatures, resultsets, Results = {}, revisions;
+let signatures, resultsets, Results = {}, revisions, res_ids_2_rev = {};
 
 function onLoad() {
     revisions = new URL(document.location).searchParams.getAll('revision');
@@ -26,7 +26,10 @@ function loadResults(responses) {
     [signatures, ...resultsets] = responses;
     let resultset_ids = [];
     resultsets.forEach(function(rs) {
-        rs.results.forEach(function (r) {resultset_ids.push(r.id);});
+        rs.results.forEach(function (r) {
+            resultset_ids.push(r.id);
+            res_ids_2_rev[r.id] = rs.meta.revision;
+        });
     });
     console.log(resultset_ids);
     let sigs = Object.keys(signatures);
@@ -57,18 +60,68 @@ function renderResults() {
     let body = document.querySelector("#container");
     body.innerHTML = '';
     let found_sigs = Object.keys(Results);
-    let test_kinds = found_sigs.map(function(sig) {
+    let rows = new Map();
+    let val_span = 0;
+    found_sigs.forEach(function(sig) {
         let test = signatures[sig];
-        let seg = [test.test || test.suite, test.test_options ? test.test_options[0] : '', test.machine_platform, test, sig];
-        return seg;
+        let name = test.test||test.suite;
+        if (test.test_options) {
+            name += ' (' + test.test_options[0] + ')';
+        }
+        if (!rows.has(name)) {
+            rows.set(name, new Map());
+        }
+        let row = rows.get(name);
+        row.set(test.machine_platform, sig);
+        let test_results = Results[sig];
+        let values = test_results.map(test => test.value);
+        let min = Math.min.apply(null, values);
+        let max = Math.max.apply(null, values);
+        val_span = Math.max(max - min, val_span);
+        let revs = new Set();
+        let results = test_results.map(function(result) {
+            let rev = res_ids_2_rev[result.result_set_id];
+            revs.add(rev);
+            return {
+                revision: rev,
+                value: result.value,
+            };
+        });
+        revs = revisions.filter(rev => revs.has(rev));
+        row.set(test.machine_platform, {
+            max: max,
+            min: min,
+            tested_revisions: revs,
+            results: results,
+        });
     });
-    test_kinds.sort();
-    test_kinds.forEach(function(t) {
-        let [label, opts, platform, test, sig] = t;
-        let row = document.createElement('tr');
-        row.innerHTML = `<td>${label}${opts ? " (" + opts + ")" : ''}</td>`;
-        body.appendChild(row);
-    })
+    val_span = Math.ceil(val_span);
+    rows = Array.from(rows);
+    rows.sort();
+    rows.forEach(function(t) {
+        let [label, platform_map] = t;
+        let platforms = Array.from(platform_map);
+        platforms.sort();
+        platforms.forEach(function (t, i) {
+            let [platform, {max, min, tested_revisions, results}] = t;
+            let lower_bound = Math.max(Math.floor((max + min - val_span) / 2), 0);
+            let domain = [lower_bound , lower_bound + val_span];
+            let row = document.createElement('tr');
+            if (i === 0) {
+                row.innerHTML = `<td rowspan="${platforms.length}">${label}</td>`;
+            }
+            row.insertAdjacentHTML('beforeend', `<td>${platform}</td>`);
+            row.insertAdjacentHTML('beforeend', `<td>${domain[0]}</td>`);
+            var data_cell = document.createElement('td');
+            results.forEach(function(result) {
+                data_cell.insertAdjacentHTML('beforeend',
+                    `<span style="color: #${result.revision.slice(6)};">${result.value}</span> `);
+            });
+            row.appendChild(data_cell);
+            row.insertAdjacentHTML('beforeend', `<td>${domain[1]}</td>`);
+            body.appendChild(row);
+        });
+    });
 }
 
 onLoad();
